146e4f25f6a0693e69691c5bd90152c2c61b532e
MdFaiyyajAlam <mdfaiyyajalam54918@gmail.com>
2020-12-30 20:07:21 +0530

    1st Day

---
+++ app/Http/Controllers/BizCommentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BizComment;
+use Illuminate\Http\Request;
+
+class BizCommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BizComment  $bizComment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BizComment $bizComment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BizComment  $bizComment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BizComment $bizComment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BizComment  $bizComment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BizComment $bizComment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BizComment  $bizComment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BizComment $bizComment)
+    {
+        //
+    }
+}
---
+++ database/factories/BizCommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BizComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BizCommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BizComment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
